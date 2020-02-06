7a7d7aa5df1808131580e2ce0dd7f194fe4d6908
northerchild <osarus-13@hotmail.com>
2020-02-06 14:51:38 -0500

    Se crea el modal y se agrega VUEX

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -31,18 +31,23 @@
       <slot></slot>
     </main>
     <footer-partial></footer-partial>
+    <modal :show="true">
+      <h1>Modal Title</h1>
+    </modal>
   </div>
 </template>
 
 <script>
 import HeaderPartial from '@/partials/HeaderPartial.vue';
 import FooterPartial from '@/partials/FooterPartial.vue';
+import Modal from '@/components/Modal.vue';
 
 export default {
   name: 'DefaultLayout',
   components: {
     HeaderPartial,
     FooterPartial,
+    Modal
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -4,9 +4,20 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    user: null,
-  },
-  mutations: {},
-  actions: {},
+    state: {
+        user: null,
+        modals: {
+            login: false,
+        },
+    },
+    mutations: {
+        SET_MODAL_STATE: (state, { name, value }) => {
+            state.modals[name] = value;
+        }
+    },
+    actions: {
+        TOGGLE_MODAL_STATE: ({ commit }, { name, value }) => {
+            commit('SET_MODAL_STATE', { name, value });
+        }
+    },
 });
